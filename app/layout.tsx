ff84db94dad6cd8462f624992dfda1f83ab2c45d
Ochygorek <117666852+Ochygorek@users.noreply.github.com>
2024-02-21 21:08:40 +0100

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,19 +21,32 @@ export const metadata: Metadata = {
     },
     url: 'https://iceterisk.com',
     description: "Iceterisk i a notes taking app that allows you to make notes, edit the thext and save them in a database storage",
+    siteName: 'Iceterisk',
     images: [
       {
-        url: 'link.svg'
+        url: 'https://iceterisk.com/link.svg',
+        width: 1200,
+        height: 630
       }
     ],
+    type: 'website',
   },
   robots: {
-    index: true,
-    follow: true
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
   },
   keywords: ['Iceterisk', 'Note', 'Notes', 'Take notes', 'Taking notes', 'Iceterisk note', 'Iceterisk notes'],
-  applicationName: 'Iceterisk.com',
-  generator: 'Iceterisk.com',
+  applicationName: 'Iceterisk.com', // ?
+  generator: 'Iceterisk.com', // ?
   twitter: {
     card: 'summary_large_image',
     title: 'Iceterisk',
