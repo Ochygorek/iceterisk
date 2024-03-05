561100852f5ff9c2e412a25ea4e629b4f0a7b48c
Ochygorek <117666852+Ochygorek@users.noreply.github.com>
2024-03-05 21:47:30 +0100

    speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navigation from "./components/navigation";
 import Footer from "./components/footer";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -75,6 +76,7 @@ export default function RootLayout({
         </div>
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
